bbb508712bc63ffce000c6218104bb2fb6ea23e6
soyebganja <soyeb.ganja@gmail.com>
2025-02-23 13:24:46 +0530

    Final code push

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 from prediction_helper import predict
 
 def main():
-  st.title("Credit Risk Modeling App")
+  st.title("Doom Finance: Credit Risk Modeling")
   
   # Row 1 - First set of controls
   col1, col2, col3 = st.columns(3)
@@ -20,38 +20,38 @@ def main():
   # Row 2 - Second set of controls
   col4, col5, col6 = st.columns(3)
   
+  loan_to_income_ratio = loan_amount / income if income > 0 else 0
   with col4:
-    loan_tenure_months = st.number_input("Loan Tenure (months)", min_value=1, max_value=360, value=36)
+    st.write("Loan to Income Ratio: ")
+    st.write(f"{loan_to_income_ratio:.2f}")
   with col5:
-    avg_dpd_per_delinquency = st.number_input("Days Past Due (DPD)", min_value=0, max_value=1000, value=20)
+    loan_tenure_months = st.number_input("Loan Tenure (months)", min_value=1, max_value=360, value=36)
   with col6:
-    credit_utilization_ratio = st.slider("Credit Utilization (%)", min_value=0, max_value=100, value=30)
+    avg_dpd_per_delinquency = st.number_input("Avg Days Past Due (DPD)", min_value=0, max_value=1000, value=20)
   
   # Row 3 - Third set of controls
   col7, col8, col9 = st.columns(3)
   
   with col7:
-    residence_type = st.selectbox("Residence Type", ["Owned", "Rented", "Mortgage"])
+    delinquency_ratio = st.number_input("Delinquency Ratio", min_value=0, max_value=100, value=30)
   with col8:
-    loan_purpose = st.selectbox("Loan Purpose", ["Education", "Home", "Auto", "Personal"])
+    credit_utilization_ratio = st.number_input("Credit Utilization (%)", min_value=0, max_value=100, value=30)
   with col9:
-    loan_type = st.selectbox("Loan Type", ["Unsecured", "Secured"])
+    open_loan_accounts = st.number_input("Open Loan Accounts", min_value=0, max_value=50, value=2)
   
   # Row 4 - New controls
   col10, col11, col12 = st.columns(3)
   
-  loan_to_income_ratio = loan_amount / income if income > 0 else 0
   with col10:
-    st.write("Loan to Income Ratio: ")
-    st.write(f"{loan_to_income_ratio:.2f}")
-    # loan_to_income_ratio = st.number_input("Loan to Income Ratio", min_value=0.0, max_value=1.0, value=0.3, format="%.2f")
+    residence_type = st.selectbox("Residence Type", ["Owned", "Rented", "Mortgage"])
   with col11:
-    delinquency_ratio = st.number_input("Delinquency Ratio", min_value=0, max_value=100, value=30)
+    loan_purpose = st.selectbox("Loan Purpose", ["Education", "Home", "Auto", "Personal"])
   with col12:
-    open_loan_accounts = st.number_input("Open Loan Accounts", min_value=0, max_value=50, value=2)
+    loan_type = st.selectbox("Loan Type", ["Unsecured", "Secured"])
+  
   
   # Submit button
-  if st.button("Submit Application"):
+  if st.button("Calculate Risk"):
     probability, credit_score, rating = predict(
       age, income, loan_amount, loan_tenure_months, avg_dpd_per_delinquency,
       delinquency_ratio, credit_utilization_ratio, open_loan_accounts,
